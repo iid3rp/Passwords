0954cb7d284dbf4137652790e2bb912b05ca27f9
derp :3 <133679016+iid3rp@users.noreply.github.com>
2023-11-15 15:33:17 +0800

    adding a rouge (Randomness) java file
    :3

---
+++ rogue.java
@@ -0,0 +1,18 @@
+public class rogue
+{
+    public static final Point[][] roguePoints =
+    {
+        { // scene #0  |  where the background is changing at any moment
+            new Point(0, 0) // example
+        },
+        {
+            // new items will be in the future sessions
+        }
+        // scenes will be in the fuutre sessions.
+    };
+    
+    public static void main(String[] args)
+    {
+        // roguelike stuff goes in this class :3
+    }
+}
