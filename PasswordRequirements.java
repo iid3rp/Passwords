3147d05db1eca05fcbba2171d864f0e61d2062c8
derp :3 <iid3rp.francismadanlo@gmail.com>
2023-11-17 14:06:25 +0800

    added PasswordRequirements.java

---
+++ PasswordRequirements.java
@@ -0,0 +1,26 @@
+public class PasswordRequirements
+{
+    public static void check(String passwordHolder)
+    {
+        passwordLowerCase = passwordHolder.toLowerCase();
+        passwordUpperCase = passwordHolder.toupperCase();
+        
+        for(int i = 0; i < /*number holder*/; i++)
+        {
+           switch(/*number holder here*/)
+           {
+               case 1:
+                   case1();
+                   break; // example
+               case 2:
+               case 3:
+               default:
+           }
+        }
+    }
+    
+    private static void case1()
+    {
+        // the code goes here :3
+    }
+}
